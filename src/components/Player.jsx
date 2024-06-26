ae6b150866cf966b81fadcb5b6c1c14a103972f6
kad <footnofetish123@gmail.com>
2024-06-26 11:56:29 -0400

    chore: Refactor Player component to use useState hook for name editing

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,31 +1,36 @@
-import {useState} from 'react';
-export const Player = ({initialName, symbol, isActive, onChangeName}) => {
-   const [playerName, setPlayerName] = useState(initialName);
-   const [isEditing, setIsEditing] = useState(false);
+import { useState } from 'react';
 
-   const handleEdit = () => {
+
+export const Player = ({ initialName, symbol, isActive, onChangeName }) => {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEdit = () => {
     setIsEditing((editing) => !editing);
-    onChangeName(symbol, playerName);
-   }
-   if(isEditing) {
+    if (isEditing) {
       onChangeName(symbol, playerName);
-   }
-   
-   const handleChange = (event) => {
-      setPlayerName(event.target.value);
-   }
-   let editablePlayerName = <span className = "player-name">{playerName}</span>
-   if (isEditing) {
-      editablePlayerName = <input type = "text" required value = {playerName} onChange = {handleChange}/>
-   }
-   return (
-    <li className= {isActive ? 'active' : undefined}>
-    <span className = "player">
-      {editablePlayerName}
-    <span className = 'player-symbol'>{symbol}</span>
-    </span> 
-    <button onClick = {handleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
-  </li>
+    }
+  };
 
-   ) 
-}
+  const handleChange = (event) => {
+    setPlayerName(event.target.value);
+  };
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li className={isActive ? 'active' : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
+};
+
